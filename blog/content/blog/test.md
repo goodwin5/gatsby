932847d8a17f7db7e40708976d87e7b9e63fd174
陈国杨 <cgy225@126.com>
2021-12-30 17:48:11 +0800

    完成菜单

---
+++ blog/content/blog/test.md
@@ -0,0 +1,16 @@
+---
+slug: "mytest1"
+title: My Test3
+date: "2021-05-28T22:40:32.169Z"
+description: This is a custom description for SEO and Open Graph purposes, rather than the default generated excerpt. Simply add a description field to the frontmatter.
+---
+# MDX Test
+
+- first
+
+```javascript
+var str = ""
+function name(params) {
+    
+}
+```
---
+++ blog/src/components/header.tsx
@@ -0,0 +1,133 @@
+import * as React from 'react'
+import './header.scss'
+
+class Header extends React.Component {
+
+    componentDidMount() {
+        // Other important pens.
+// Map: https://codepen.io/themustafaomar/pen/ZEGJeZq
+// Dashboard: https://codepen.io/themustafaomar/pen/jLMPKm
+
+let dropdowns = document.querySelectorAll('.navbar .dropdown-toggler')
+let dropdownIsOpen = false
+
+// Handle dropdown menues
+if (dropdowns.length) {
+  // Usually I don't recommend doing this (adding many event listeners to elements have the same handler)
+  // Instead use event delegation: https://javascript.info/event-delegation
+  // Why: https://gomakethings.com/why-event-delegation-is-a-better-way-to-listen-for-events-in-vanilla-js
+  // But since we only have two dropdowns, no problem with that. 
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', (event) => {
+      let target = document.querySelector(`#${event.target.dataset.dropdown}`)
+
+      if (target) {
+        if (target.classList.contains('show')) {
+          target.classList.remove('show')
+          dropdownIsOpen = false
+        } else {
+          target.classList.add('show')
+          dropdownIsOpen = true
+        }
+      }
+    })
+  })
+}
+
+// Handle closing dropdowns if a user clicked the body
+window.addEventListener('mouseup', (event) => {
+  if (dropdownIsOpen) {
+    dropdowns.forEach((dropdownButton) => {
+      let dropdown = document.querySelector(`#${dropdownButton.dataset.dropdown}`)
+      let targetIsDropdown = dropdown == event.target
+
+      if (dropdownButton == event.target) {
+        return
+      }
+
+      if ((!targetIsDropdown) && (!dropdown?.contains(event.target))) {
+        dropdown?.classList.remove('show')
+      }
+    })
+  }
+})
+
+// Open links in mobiles
+function handleSmallScreens() {
+  document.querySelector('.navbar-toggler')
+    .addEventListener('click', () => {
+    let navbarMenu = document.querySelector('.navbar-menu')
+
+    if (navbarMenu.style.display === 'flex') {
+      navbarMenu.style.display = 'none'
+      return
+    }
+
+    navbarMenu.style.display = 'flex'
+  })
+}
+
+handleSmallScreens()
+
+    }
+
+    render() {
+        return (
+            <nav className="navbar">
+            <div className="container">
+          
+              <div className="navbar-header">
+                <button className="navbar-toggler" data-toggle="open-navbar1">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+                <a href="#">
+                  <h5>Awesome<span>logo</span></h5>
+                </a>
+              </div>
+          
+              <div className="navbar-menu" id="open-navbar1">
+                <ul className="navbar-nav">
+                  <li className="active"><a href="#">Home</a></li>
+                  <li className="navbar-dropdown">
+                    <a href="#" className="dropdown-toggler" data-dropdown="my-dropdown-id">
+                      Categories <i className="fa fa-angle-down"></i>
+                    </a>
+                    <ul className="dropdown" id="my-dropdown-id">
+                      <li><a href="#">Actions</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li className="separator"></li>
+                      <li><a href="#">Seprated link</a></li>
+                      <li className="separator"></li>
+                      <li><a href="#">One more seprated link.</a></li>
+                    </ul>
+                  </li>
+                  <li className="navbar-dropdown">
+                    <a href="#" className="dropdown-toggler" data-dropdown="blog">
+                      Blog <i className="fa fa-angle-down"></i>
+                    </a>
+                    <ul className="dropdown" id="blog">
+                      <li><a href="#">Some category</a></li>
+                      <li><a href="#">Some another category</a></li>
+                      <li className="separator"></li>
+                      <li><a href="#">Seprated link</a></li>
+                      <li className="separator"></li>
+                      <li><a href="#">One more seprated link.</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Signin</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        )
+    }
+
+
+}
+
+
+export default Header
